e0651bc2fc2b9672714b08ccb0046704758586ff
abdulhalimaliakbar <35986447+abdulhalimaliakbar@users.noreply.github.com>
2018-01-31 20:47:38 +0800

    Update gcc48.rb

--- gcc48.rb
+++ gcc48.rb
@@ -1,4 +1,4 @@
-class GccAT48 < Formula
+class Gcc48 < Formula
   desc "GNU compiler collection"
 
   def arch
@@ -37,7 +37,7 @@ class GccAT48 < Formula
     # Fixes build on El Capitan
     # https://trac.macports.org/ticket/48471
     patch :p0 do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/dcfc5a2e6/gccat48/define_non_standard_clang_macros.patch"
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/dcfc5a2e6/gcc48/define_non_standard_clang_macros.patch"
       sha256 "e727383c9186fdc36f804c69ad550f5cfd2b996e37083be94c0c9aa8fde226ee"
     end
     # Fixes build with Xcode 7.
